8e8cb0e1c1e0dc64e57f1c66182fbb6638eeaed5
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-12 11:35:45 +0100

    showing members

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -2,8 +2,16 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
 export const PageWelcome = () => {
-	const { message } = useContext(AppContext);
+	const { message, members } = useContext(AppContext);
 	return (
-		<p>{message}</p>
-	)
-}
+		<>
+			<p>{message}</p>
+			<h2 className="text-xl mt-3 mb-2">Members</h2>
+			<ul className="list-disc ml-6">
+				{members.map((member) => {
+					return <li key={member.id}>{member.name}</li>;
+				})}
+			</ul>
+		</>
+	);
+};
