3ceddd56190bacd97c3b33914afd3a6ddc4a0c19
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-12 11:19:03 +0100

    init commit

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -3,7 +3,6 @@ import { AppContext } from "../AppContext";
 
 export const PageWelcome = () => {
 	const { message } = useContext(AppContext);
-	console.log(111, message);
 	return (
 		<p>{message}</p>
 	)
