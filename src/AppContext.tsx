0fe9c84778e9ffc1e741f54905cd6a89840390e5
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-12 11:32:08 +0100

    appContext finished

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,22 +1,38 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from "react";
+import { IMember } from "./interfaces";
+import axios from "axios";
 
 interface IAppContext {
 	message: string;
+	members: IMember[];
 }
 
 interface IAppProvider {
 	children: React.ReactNode;
 }
 
+const backendUrl = "http://localhost:3311";
+
 export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
-	const message = 'Welcome to this site.';
+	const [members, setMembers] = useState<IMember[]>([]);
+
+	const message = "Welcome to this site.";
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${backendUrl}/members`);
+			const _members = response.data;
+			setMembers(_members);
+		})();
+	}, []);
 
 	return (
 		<AppContext.Provider
 			value={{
-				message
+				message,
+				members,
 			}}
 		>
 			{children}
